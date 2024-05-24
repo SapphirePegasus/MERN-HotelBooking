0c533ba78db6259b9e0edc36d31de07aa25013e7
Prittam Bhattacharyya <59970935+SapphirePegasus@users.noreply.github.com>
2024-05-24 17:30:46 +0530

    Add files via upload

---
+++ api/routes/hotels.js
@@ -0,0 +1,42 @@
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+/*router.get("/"),async (req,res)=>{
+  try{
+    const hotels=await Hotel.find();
+    res.status(200).json(hotels);
+  }catch (err){
+    res.status(500).json(err);
+  }
+}*/
+
+export default router;
